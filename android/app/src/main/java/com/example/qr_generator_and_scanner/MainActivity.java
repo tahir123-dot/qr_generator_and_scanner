ca4c8ae1a7adec6f2fdac82dd35ed8746d03805f
Tahir Rashid <tahirbhai736333@gmail.com>
2026-03-01 08:13:31 +0500

    initial commit

---
+++ android/app/src/main/java/com/example/qr_generator_and_scanner/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.qr_generator_and_scanner;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Code Scanner and Generator',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AR code scanner and generator'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: Text('Scan QR Code'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
